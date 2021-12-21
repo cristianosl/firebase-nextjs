e5c4494b17cd10665bff5fc5e88f5070653559a9
Cristiano Lima <cristianosl@gmail.com>
2021-12-21 01:20:37 -0300

    firebase messaing sw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,40 @@ import Head from 'next/head'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../store'
 import { increment, decrement } from '../store/queue'
+import { getMessaging, getToken, Messaging } from "firebase/messaging";
+import { useEffect, useState } from 'react'
+import { firebaseApp } from '../config/firebase'
+
 
 const Home: NextPage = () => {
   const count = useSelector((state: RootState) => state.counter.value)
   const dispatch = useDispatch()
+  const [messaging, setMessaging] = useState<Messaging>()
 
+  useEffect(() => {
+    setMessaging(getMessaging(firebaseApp))
+    if (messaging) {
+      console.log('process.env.NEXT_PUBLIC_FIREBASE_FCM_KEY_PAIR', process.env.NEXT_PUBLIC_FIREBASE_FCM_KEY_PAIR)
+      getToken(messaging, { vapidKey: process.env.NEXT_PUBLIC_FIREBASE_FCM_KEY_PAIR })
+        .then((currentToken) => {
+          console.log('oi')
+          if (currentToken) {
+            // Send the token to your server and update the UI if necessary
+            // ...
+            console.log('currentToken', currentToken)
+          } else {
+            // Show permission request UI
+            console.log('No registration token available. Request permission to generate one.');
+            // ...
+          }
+        }).catch((err) => {
+          console.log('falhou')
+          console.log('An error occurred while retrieving token. ', err);
+          // ...
+        });
+    }
+
+  }, [messaging])
 
 
   return (
