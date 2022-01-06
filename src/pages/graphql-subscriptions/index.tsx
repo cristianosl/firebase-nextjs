6000789b82a0502dc186123a6f2f5dd4deb4ff72
Cristiano Lima <cristiano.l@dasa.com.br>
2022-01-05 22:54:33 -0300

    graphql com subscriptions

--- src/pages/graphql-subscriptions/index.tsx
+++ src/pages/graphql-subscriptions/index.tsx
@@ -4,42 +4,48 @@ import { useEffect } from 'react'
 import { useAppDispatch } from '../../store/hooks'
 import QueuePositions from '../../components/QueuePositions'
 import Link from 'next/link'
-import { gql, useSubscription } from '@apollo/client';
+import { ApolloProvider, gql, useSubscription } from '@apollo/client';
+import { IQueuePosition } from '../../types/QueuePosition'
+import { updateQueue } from '../../store/queue'
+import { getApolloClient } from '../../services/getApolloClient'
 
-const USER_ADDED = gql`
-  subscription UserAdded {
-    userAdded {
-      _id
-      name
-      email
-      active
+const GET_QUEUE_BY_PATIENT_ID = gql`
+  subscription GetQueueByPatientId($patientId: String!) {
+    getQueueByPatientId(patientId: $patientId) {
+      queueId
+      patientId
+      status
+      position
+      updatedAt
+      attendanceId
     }
   }
 `;
-const GET_USERS = gql`
-  query Users {
-    users {
-      _id
-      name
-      email
-      active
-    }
-  }
-`
 
-const GraphqlSubscriptions: NextPage = () => {
-
-
+const GraphqlSubscriptions: React.VFC = () => {
   const dispatch = useAppDispatch()
   const { data, loading, error } = useSubscription(
-    USER_ADDED
+    GET_QUEUE_BY_PATIENT_ID, {
+    variables: {
+      patientId: "123123"
+    }
+  }
   );
   // const { loading, error, data } = useQuery(GET_USERS);
   useEffect(() => {
-    console.log('data', data)
-    console.log('loading', loading)
-    console.log('error', error)
-  }, [data, loading, error])
+    if (data && !loading) {
+      const { getQueueByPatientId: { attendanceId, queueId, position, status, updatedAt } } = data
+      const queuePosition: IQueuePosition = {
+        attendanceId: attendanceId,
+        id: queueId,
+        position: position,
+        status: status,
+        updatedAt: updatedAt
+      }
+      dispatch(updateQueue(queuePosition))
+      console.log('data', data)
+    }
+  }, [data, loading, error, dispatch])
   return (
     <div className="container">
       <Head>
@@ -51,7 +57,7 @@ const GraphqlSubscriptions: NextPage = () => {
       <nav aria-label="breadcrumb">
         <ol className="breadcrumb">
           <li className="breadcrumb-item"><Link href="/">Home</Link></li>
-          <li className="breadcrumb-item active" aria-current="page">Firestore</li>
+          <li className="breadcrumb-item active" aria-current="page">Graphql Subscriptions</li>
         </ol>
       </nav>
       <h2>Graphql Subscriptions</h2>
@@ -60,4 +66,7 @@ const GraphqlSubscriptions: NextPage = () => {
   )
 }
 
-export default GraphqlSubscriptions
+const GraphqlSubscriptionsWithApollo: NextPage = () => <ApolloProvider client={getApolloClient()}>
+  <GraphqlSubscriptions />
+</ApolloProvider>
+export default GraphqlSubscriptionsWithApollo
